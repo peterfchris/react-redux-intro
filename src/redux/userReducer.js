094ff90f8aed43f51c5219ce9efe1d436a6a839c
Peter Christensen <peter.f.chris@gmail.com>
2019-05-29 10:19:28 -0600

    first commit

---
+++ src/redux/userReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    loading: false,
+    user: {}
+}
+
+const GET_USER = "GET_USER"
+
+
+function userReducer(state = initialState, action) {
+    return state
+}
+
+export default userReducer
